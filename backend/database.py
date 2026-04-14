3eede2859513c6ba457cab140ad67eed8b05f69c
Vespertino <Vespertino@C14PC21.informatica.comercio>
2026-04-14 20:27:03 +0200

    Iniciando con la integracion del logging, cambiar bbdd a PostGreSQL - 2

--- backend/database.py
+++ backend/database.py
@@ -3,12 +3,10 @@ from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 import datetime
 
-# Ruta para la DB
-SQLALCHEMY_DATABASE_URL = "sqlite:///./backend/chatbot_plan_lector.db"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:1234@localhost:5432/chatbot_db"
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
